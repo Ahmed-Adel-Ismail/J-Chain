771cfa0638ddd5487c80c4360d6abe6e03f14190
Ahmed Adel Ismail <ahm3d.ad3l@gmail.com>
2017-11-20 18:04:50 +0200

    added some sample code in test class to check the validity of gradle dependency

---
+++ myapplication/src/test/java/ahmedadekismail/myapplication/MainActivityTest.java
@@ -0,0 +1,28 @@
+package ahmedadekismail.myapplication;
+
+import com.chaining.Chain;
+
+import org.junit.Test;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Created by Ahmed Adel Ismail on 11/20/2017.
+ */
+public class MainActivityTest {
+
+    @Test
+    public void main() {
+        Chain
+                .call(new Callable<Integer>() {
+                    @Override
+                    public Integer call() throws Exception {
+                        return 10;
+                    }
+                })
+                .and(20)
+                .collect()
+                .call();
+    }
+
+}
